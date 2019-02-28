2fb803e3691e35c60a01967edb53c08eeac2df5c
leeapps <33534006+leeapps@users.noreply.github.com>
2019-02-28 15:17:59 +1100

    Update guardians.ps1

--- guardians.ps1
+++ guardians.ps1
@@ -41,14 +41,84 @@ catch
     return
 }
 $ReportArray=@()
-write-host "Getting a list of VM's from the Rubrik Cluster" -ForegroundColor Green
-add-content $logfile "Getting a list of VM's from the Rubrik Cluster"
-$RubrikVMs = Get-Rubrikvm | Select Name, effectiveSlaDomainName,hostName,clusterName,id
+
+$PromptTitle = "Do you want to Filter by SLA Domain?"
+$PromptMessage = "Do you want to see all entries or Filter by SLA Domain?" 
+$Filter = New-Object System.Management.Automation.Host.ChoiceDescription "&Filter", "Filters the results"
+$All = New-Object System.Management.Automation.Host.ChoiceDescription "&all", "Shows all the Results"
+$PromptOptions = [System.Management.Automation.Host.ChoiceDescription[]]($Filter, $All)
+$PromptResult = $host.ui.PromptForChoice($PromptTitle, $PromptMessage, $PromptOptions, 0) 
+
+switch ($PromptResult)
+    {
+        0 {"Filtering the results"}
+        1 {"Showing All Results"}
+    }
+if ($PromptResult -eq 0)
+{
+	$SLAList = Get-RubrikSLA | Select Name
+	Add-Type -AssemblyName System.Windows.Forms
+	Add-Type -AssemblyName System.Drawing
+
+	$form = New-Object System.Windows.Forms.Form
+	$form.Text = 'Select a SLA'
+	$form.Size = New-Object System.Drawing.Size(300,200)
+	$form.StartPosition = 'CenterScreen'
+
+	$OKButton = New-Object System.Windows.Forms.Button
+	$OKButton.Location = New-Object System.Drawing.Point(75,120)
+	$OKButton.Size = New-Object System.Drawing.Size(75,23)
+	$OKButton.Text = 'OK'
+	$OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+	$form.AcceptButton = $OKButton
+	$form.Controls.Add($OKButton)
+
+	$CancelButton = New-Object System.Windows.Forms.Button
+	$CancelButton.Location = New-Object System.Drawing.Point(150,120)
+	$CancelButton.Size = New-Object System.Drawing.Size(75,23)
+	$CancelButton.Text = 'Cancel'
+	$CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+	$form.CancelButton = $CancelButton
+	$form.Controls.Add($CancelButton)
+
+	$label = New-Object System.Windows.Forms.Label
+	$label.Location = New-Object System.Drawing.Point(10,20)
+	$label.Size = New-Object System.Drawing.Size(280,20)
+	$label.Text = 'Please select a SLA:'
+	$form.Controls.Add($label)
+
+	$listBox = New-Object System.Windows.Forms.ListBox
+	$listBox.Location = New-Object System.Drawing.Point(10,40)
+	$listBox.Size = New-Object System.Drawing.Size(260,20)
+	$listBox.Height = 80
+	foreach ($SLA in $SLAList) {
+		[void] $listBox.Items.Add($SLA.Name)
+	}
+
+	$form.Controls.Add($listBox)
+
+	$form.Topmost = $true
+
+	$result = $form.ShowDialog()
+
+	if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+	{
+		$SelectedSLA = $listBox.SelectedItem
+		$RubrikVMs = Get-Rubrikvm -SLA $SelectedSLA | Select Name, effectiveSlaDomainName,hostName,clusterName,id
+	}	
+	}
+Else
+{
+	write-host "Getting a list of VM's from the Rubrik Cluster" -ForegroundColor Green
+	add-content $logfile "Getting a list of VM's from the Rubrik Cluster"
+	$RubrikVMs = Get-Rubrikvm | Select Name, effectiveSlaDomainName,hostName,clusterName,id
+}
 write-host "Getting a list of Missed Snapshots for all VM's" -ForegroundColor Green
 add-content $logfile "Getting a list of Missed Snapshots for all VM's"
 write-host "If you have a large number of VM's this will take some time to process" -ForegroundColor Green
 foreach ($vm in $RubrikVMs) {
 	$VMName = $vm.Name
+	write-host "Processing " $VMName -ForegroundColor Green
 	$SLADomain = $vm.effectiveSlaDomainName
 	$ClusterName = $vm.clusterName
 	$CurrentHost = $vm.hostName
